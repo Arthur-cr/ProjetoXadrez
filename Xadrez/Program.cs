e2ec06cd3efd13cb09b38cc45badea8b941b1f3c
Arthur <arthurcaetano.acrr@gmail.com>
2023-12-14 15:29:23 -0300

    Crinando a class Posicao

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using Tabuleiro;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("pisocao: " + P);
+
+
+
+        }
+    }
+}
